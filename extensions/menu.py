ea544e43eb97df97d3191a7283725e7ee3262241
darksan <evancavaraujo3@gmail.com>
2022-01-14 20:03:06 -0300

    Adicionado o menu dropdown, ainda falta algumas interações dele

--- extensions/menu.py
+++ extensions/menu.py
@@ -1,5 +1,4 @@
-from datetime import datetime
-from sys import prefix
+import asyncio
 
 import hikari
 import lightbulb
@@ -8,7 +7,7 @@ menu_plugin= lightbulb.Plugin("Menu")
 
 @menu_plugin.command()
 @lightbulb.command("menu",description="Um menu para ajudar as pessoas sobre algumas coisas do Bot")
-@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand) 
+@lightbulb.implements(lightbulb.PrefixCommandGroup, lightbulb.SlashCommand) 
 async def menu(ctx: lightbulb.Context)-> None:
     menu = hikari.Embed(
         title="**Comandos e funções**",
@@ -16,10 +15,10 @@ async def menu(ctx: lightbulb.Context)-> None:
         
     )
     menu.add_field(
-        name=f"{prefix}roll", value="Use ele para rolar dados. Ex: 1d20", inline=False
+        name=f"+roll", value="Use ele para rolar dados. Ex: 1d20", inline=False
     )
     menu.add_field(
-        name="#menu", value="Use ele para acessar esse menu aqui", inline=True
+        name="+menu", value="Use ele para acessar esse menu aqui", inline=True
     )
     menu.add_field(
         name="#sistemas",
@@ -38,6 +37,45 @@ async def menu(ctx: lightbulb.Context)-> None:
     #    icon_url=ctx.author.avatar_url,
     #)
     await ctx.respond(embed=menu)
+
+MENUDROP ={
+    "SISTEMA": "📒",
+    "DICE":"🎲"
+}
+@menu.child
+@lightbulb.command("drop", description="Um menu em dropdown")
+@lightbulb.implements(lightbulb.PrefixSubGroup)
+async def drop(ctx: lightbulb.Context) -> None:
+    select_menu =(
+        ctx.bot.rest.build_action_row()
+        .add_select_menu("menu_dropdown")
+        .set_placeholder("Selecione para")
+    )
+
+    for name, emoji in MENUDROP.items():
+        select_menu.add_option(
+            name,
+            name.lower().replace(" ","_"),
+
+        ).set_emoji(emoji).add_to_menu()
+    resp = await ctx.respond(
+        "Selecione um elemento no menu",
+        component=select_menu.add_to_container(),
+    )
+    msg = await resp.message()
+    
+    try:
+        evento = await ctx.bot.wait_for(
+            hikari.InteractionCreateEvent,
+            timeout=60,
+            predicate=lambda e:
+                isinstance(e.interaction, hikari.ComponentInteraction)
+                and e.interaction.user.id == ctx.author.id
+                and e.interaction.message.id == msg.id
+                and e.interaction.component_type == hikari.ComponentType.SELECT_MENU
+        )
+    except asyncio.TimeoutError:
+        await msg.edit("O tempo expirou", component=[])    
 
 def load(bot):
     bot.add_plugin(menu_plugin)
