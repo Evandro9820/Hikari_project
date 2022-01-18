ac891cd81371472748d80b3bdba6ad9f3f7e4055
darksan <evancavaraujo3@gmail.com>
2022-01-17 22:24:21 -0300

    Add novas extenções, e remoção de algumas.

--- extensions/mod.py
+++ extensions/mod.py
@@ -1,4 +1,7 @@
+import asyncio
+from binhex import REASONABLY_LARGE
 import datetime
+from typing_extensions import Required
 import hikari
 import lightbulb
 
@@ -10,7 +13,7 @@ mod_plugin = lightbulb.Plugin("Mod")
 @lightbulb.add_checks(lightbulb.has_guild_permissions(hikari.Permissions.KICK_MEMBERS))
 @lightbulb.option("member", "O membro que será removido", hikari.Member)
 @lightbulb.option("reason", "O motivo da remoção.", required=False)
-@lightbulb.command("kick", "Kick o membro.")
+@lightbulb.command("kick", description="Kick o membro.")
 @lightbulb.implements(lightbulb.commands.SlashCommand)
 async def cmd_kick(ctx: lightbulb.context.SlashContext) -> None:
     member = ctx.options.member
@@ -19,8 +22,50 @@ async def cmd_kick(ctx: lightbulb.context.SlashContext) -> None:
     await ctx.respond(f"Kicked `{member.display_name}` for reason `{reason}`.")
 
 
+""" @mod_plugin.command()
+@lightbulb.add_checks(lightbulb.has_guild_permissions(hikari.Permissions.BAN_MEMBERS))
+@lightbulb.option("Membro","Que você irá banir", hikari.User)
+@lightbulb.option("Motivo", "O motivo do banimento", required=False)
+@lightbulb.command("ban",description="Banir membro do servidor")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def cmd_ban(ctx: lightbulb.context.SlashContext) -> None:
+    delete_message_days = ctx.options.delete_message_days
+    if not 0 <= delete_message_days <= 7:
+        await ctx.respond("delete mensagens de 0 a 7 dias.")
+        return
+    user = ctx.options.user
+    reason = ctx.options.reason
+    await ctx.get_guild().ban(
+        user, delete_message_days=delete_message_days, reason=reason
+    )
+    await ctx.respond(
+        f"Foi `{user.username}` pelo motivo `{reason}` "
+        f"e foi deletado mensagens de `{delete_message_days}` dias atrás."
+    )
+ """
 
+@mod_plugin.command
+@lightbulb.option(
+    "messages", "The number of messages to purge.", type=int, required=True
+)
+@lightbulb.command("purge", "Purge messages.", aliases=["clear"])
+@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+async def purge_messages(ctx: lightbulb.Context) -> None:
+    num_msgs = ctx.options.messages
+    channel = ctx.channel_id
 
+    # If the command was invoked using the PrefixCommand, it will create a message
+    # before we purge the messages, so you want to delete this message first
+    if isinstance(ctx, lightbulb.PrefixContext):
+        await ctx.event.message.delete()
+
+    msgs = await ctx.bot.rest.fetch_messages(channel).limit(num_msgs)
+    await ctx.bot.rest.delete_messages(channel, msgs)
+
+    resp = await ctx.respond(f"{len(msgs)} messages deleted")
+
+    await asyncio.sleep(5)
+    await resp.delete()
 def load(bot):
     bot.add_plugin(mod_plugin)
 
---
+++ extensions/nsfw.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+from distutils import errors
+
+import hikari
+import lightbulb
+import aiohttp
+
+nsfw_plugin = lightbulb.Plugin("nfsw")
+
+
+@nsfw_plugin.command
+@lightbulb.add_checks(lightbulb.nsfw_channel_only)
+@lightbulb.option("tags", "A(s) que você irá procurar.")
+@lightbulb.command("rule34", "Procurar por post no Rule34.")
+@lightbulb.implements(lightbulb.commands.SlashCommand)
+async def cmd_rule34(ctx: lightbulb.context.SlashContext) -> None:
+    async with aiohttp.request(
+        "GEt",
+        "https://api.rule34.xxx/index.php",
+        params={
+            "page": "dapi",
+            "s": "post",
+            "q": "index",
+            "json": 1,
+            "limit": 1,
+            "tags": ctx.options.tags.replace(" ", "+"),
+        },
+    ) as resp:
+        json = await resp.json()
+        if json is None:
+            await ctx.respond("Não há nenhum post com essa tag(s).")
+            return
+        post = json[0]
+    await ctx.respond(post["sample_url"])
+
+
+@nsfw_plugin.command()
+@lightbulb.add_checks(lightbulb.nsfw_channel_only)
+@lightbulb.option("query", "The query to search for.")
+@lightbulb.command("porn", "Search a video on eporner.")
+@lightbulb.implements(lightbulb.commands.SlashCommand)
+async def cmd_porn(ctx: lightbulb.context.SlashContext) -> None: 
+    async with aiohttp.request(
+        "GET","https://www.eporner.com/api/v2/video/search/",
+        params={"per_page": 1, "query": ctx.options.query.replace(" ", "+")},
+    ) as resp:
+        json = await resp.json()
+        if json is None:
+            await ctx.respond("No video was found with that query.")
+            return
+        video = json["videos"][0]
+    await ctx.respond(video["url"])
+
+def load(bot):
+    bot.add_plugin(nsfw_plugin)
+
+
+def unload(bot):
+    bot.remove_plugin(nsfw_plugin)
